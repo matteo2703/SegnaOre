28069a165807a1706c8b2e8c7407a6ec82a5ca40
matte <matteofranchin2703@gmail.com>
2022-01-13 18:38:56 +0100

    impostazione ore

--- app/src/main/java/com/segnaore/AppDatabase.java
+++ app/src/main/java/com/segnaore/AppDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Giornata.class},version = 1)
+@Database(entities = {Giornata.class},version = 2)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final String DB_NAME = "giornate_db";
--- app/src/main/java/com/segnaore/MainActivity.java
+++ app/src/main/java/com/segnaore/MainActivity.java
@@ -1,9 +1,17 @@
 package com.segnaore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -11,6 +19,8 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     String[] mesi = {"Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"};
+    private String matt, pome;
+    private double oreMattina, orePomeriggio = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +30,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //ottenere la data di oggi
-        TextView data = findViewById(R.id.data);
+        final TextView data = findViewById(R.id.data);
         String dataAttuale = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)+" "+mesi[Calendar.getInstance().get(Calendar.MONTH)]+" "+Calendar.getInstance().get(Calendar.YEAR);
         data.setText(dataAttuale);
 
         //ottenere le ore del mese corrente
-        AppDatabase db = AppDatabase.getInstance(this);
+        final AppDatabase db = AppDatabase.getInstance(this);
         ArrayList<Giornata> list = new ArrayList<>(db.giornataDao().getMese(Calendar.getInstance().get(Calendar.MONTH)));
-        TextView totale = findViewById(R.id.totale);
+        final TextView totale = findViewById(R.id.totale);
         int oreTotali=0;
         for (int i=0;i<list.size();i++){
             oreTotali+=list.get(i).getOre_doppie();
@@ -39,7 +49,50 @@ public class MainActivity extends AppCompatActivity {
         }
         totale.setText(tot);
 
-        //TODO impostare per inserire le ore della mattina e del pomeriggio
-        //TODO aggiungere salvataggio e modifica
+        //inserire ore per mattina e pomeriggio
+        //inizializzazione componenti
+        final AutoCompleteTextView mattina, pomeriggio;
+        mattina = findViewById(R.id.mattina);
+        pomeriggio = findViewById(R.id.pomeriggio);
+        mattina.setInputType(InputType.TYPE_NULL);
+        pomeriggio.setInputType(InputType.TYPE_NULL);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_dropdown_item_1line, getResources().getStringArray(R.array.ore));
+        mattina.setAdapter(arrayAdapter);
+        pomeriggio.setAdapter(arrayAdapter);
+        mattina.setOnFocusChangeListener((v, hasFocus) ->mattina.showDropDown());
+        mattina.setOnClickListener(v -> {
+            mattina.showDropDown();
+            hideSoftKeyboard(this);
+        });
+        mattina.setOnItemClickListener((parent, view, position, id) ->matt = (String) parent.getItemAtPosition(position));
+        pomeriggio.setOnFocusChangeListener((v, hasFocus) ->pomeriggio.showDropDown());
+        pomeriggio.setOnClickListener(v -> {
+            pomeriggio.showDropDown();
+            hideSoftKeyboard(this);
+        });
+        pomeriggio.setOnItemClickListener((parent, view, position, id) ->pome = (String) parent.getItemAtPosition(position));
+
+        final Button salva=findViewById(R.id.salva);
+        //TODO salvare le ore nel giorno stabilito e permettere la modifica
+        salva.setOnClickListener(v -> {
+            if (matt!=null){
+                oreMattina = Double.parseDouble(matt);
+            }
+            if (pome!=null){
+                orePomeriggio=Double.parseDouble(pome);
+            }
+
+            Toast.makeText(this, "Ore totali di oggi: "+(oreMattina+orePomeriggio), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    public void hideSoftKeyboard(Activity activity){
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View view = activity.getCurrentFocus();
+        if(view==null){
+            view=new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
